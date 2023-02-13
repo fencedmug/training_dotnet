bb56ac89a0cde47eeede2558a2c8c8c641c5053f
fencedmug <5756508+fencedmug@users.noreply.github.com>
2023-02-13 22:42:01 +0800

    added example for ModuleInitializer attribute

---
+++ src/LanguageFeatures/ExampleClass.cs
@@ -0,0 +1,18 @@
+﻿namespace LanguageFeatures;
+
+public class ExampleClass
+{
+    // this tells dotnet to run this method when application starts
+    // can be useful if we need something to run before main executes
+    [System.Runtime.CompilerServices.ModuleInitializer]
+    public static void WillInvokeAtRuntime()
+    {
+        Console.WriteLine($"Method '{nameof(WillInvokeAtRuntime)}' invoked!");
+    }
+
+    [System.Runtime.CompilerServices.ModuleInitializer]
+    public static void AutoRun()
+    {
+        Console.WriteLine($"Method '{nameof(AutoRun)}' invoked!");
+    }
+}
