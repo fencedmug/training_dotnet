67863bf62a3e7f05cf8c9699c9ef9df69f14ba88
fencedmug <5756508+fencedmug@users.noreply.github.com>
2023-02-15 22:21:06 +0800

    added examples for Interfaces and FloatingPoints

---
+++ src/LanguageFeatures/FloatingPoints.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures;
+
+public class FloatingPoints
+{
+    [ModuleInitializer]
+    public static void Run()
+    {
+        // demo floating point values
+        // might help you to decide which type to use
+
+        var counts = 100;
+
+        float floatVal = Enumerable.Range(0, counts).Select(x => 0.000_000_000_000_1f).Sum();
+        Console.WriteLine($"Float: {floatVal:F30}");
+
+        double dblVal = Enumerable.Range(0, counts).Select(x => 0.000_000_000_000_1).Sum();
+        Console.WriteLine($"Double: {dblVal:F30}");
+
+        // be aware that decimal calculations can be 10 times slower than double
+        decimal dmVal = Enumerable.Range(0, counts).Select(x => 0.000_000_000_000_1m).Sum();
+        Console.WriteLine($"Decimal: {dmVal}");
+
+        Console.WriteLine($"Float: {1.0f / 3.3f:F30}");
+        Console.WriteLine($"Double: {1.0 / 3.3:F30}");
+        Console.WriteLine($"Decimal: {1.0m / 3.3m}");
+    }
+}
---
+++ src/LanguageFeatures/Interfaces.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures;
+
+// demostrating default implementations
+public interface DemoInterface
+{
+    void RequireImplementation();
+
+    // default implementations can be useful when designing/updating libraries
+    // consumers of new version can choose to ignore implementing new methods
+    string WhatIsMyName() => "Nobody";
+}
+
+public class DemoUseCase : DemoInterface
+{
+    public void RequireImplementation()
+    {
+        // need to do something
+    }
+
+    // compilable even when we omit the below
+    // public string WhatIsMyName()
+}
+
+public class DemoUseCase2 : DemoInterface
+{
+    public void RequireImplementation()
+    {
+        // required
+    }
+
+    // default implementation will be ignored if we implement this method
+    public string WhatIsMyName()
+    {
+        return "DemoUseCase2";
+    }
+}
