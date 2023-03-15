67ad814cd348b02adbd8a00844f2e7cce975debf
fencedmug <5756508+fencedmug@users.noreply.github.com>
2023-03-15 22:59:35 +0800

    added Distinct + OrderBy queries

--- src/EntityFrameworkCore/Database/Queries_Select.cs
+++ src/EntityFrameworkCore/Database/Queries_Select.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ public static class Queries_Select
     {
         Console.WriteLine();
         Console.WriteLine("Examples: Select Queries");
+
 
         using var ctx = new ItemContext();
         IQueryable<Item> query = ctx.Items;
@@ -30,28 +32,57 @@ public static class Queries_Select
         query = ctx.Items.Where(item => item.Created < DateTime.Now && item.Id == 300);
         Consolex.WriteLine(3, query);
 
-        query = ctx.Items.Where(item => item.Created < DateTime.Now).Where(item =>item.Id == 300);
+        // break up Where clauses -> doesn't seem to affect SQL generated
+        query = ctx.Items.Where(item => item.Created < DateTime.Now).Where(item => item.Id == 300);
         Consolex.WriteLine(4, query);
 
+        Run_Distinct_OrderBy_Queries(ctx);
+        Run_Custom_OrderBy_Queries(ctx);
+    }
 
+    private static void Run_Distinct_OrderBy_Queries(ItemContext ctx)
+    {
+        // if we have logging framework added, entity framework
+        // will complain about the calling order of Distinct + OrderBy
+        // the generated query will not show "ORDER BY"
+        var wrongQuery = ctx.Items
+            .Select(item => new { item.Name, item.Created })
+            .OrderBy(it => it.Created)
+            .Distinct();
+
+        Consolex.WriteLine("Wrong ORDER BY + DISTINCT", wrongQuery);
+
+        // the SQL generated will show "ORDER BY" but
+        // as of Mar 2023, the generated SQL doesn't seem to be "right"
+        // the following would look more "right"
+        // SELECT DISTINCT ...
+        // FROM TABLE
+        // ORDER BY Created
+        var correctQuery = ctx.Items
+            .Select(item => new { item.Name, item.Created })
+            .Distinct()
+            .OrderBy(it => it.Created);
+
+        Consolex.WriteLine("Correct DISTINCT + ORDER BY", correctQuery);
+    }
+    private static void Run_Custom_OrderBy_Queries(ItemContext ctx)
+    {
         // this query demonstrate query that requires a custom order by
         // make sure to double check query generated
         // there used to be a bug with how the query was generated in
         // earlier versions of entity framework
-        query = ctx.Items.Select(item => new
-                {
-                    item = item,
-                    sort = 
-                        item.EnumValue == "Pending" ? 0 :
-                        item.EnumValue == "Accepted" ? 1 :
-                        item.EnumValue == "Rejected" ? 2 : 3
-                })
-                .OrderBy(anon => anon.sort)
-                .Select(anon => anon.item);
+        var query = ctx.Items
+            .Select(item => new
+            {
+                item,
+                sortIndex =
+                    item.EnumValue == "Pending" ? 0 :
+                    item.EnumValue == "Accepted" ? 1 :
+                    item.EnumValue == "Rejected" ? 2 : 3
+            })
+            .OrderBy(anon => anon.sortIndex)
+            .Select(anon => anon.item);
+
         Consolex.WriteLine("Custom OrderBy", query);
-
-
     }
-
-    
 }
